185e825b8973f5159c17b3e1f17c3a880ca9d6a4
Naveed Ausaf <naveed.ausaf@gmail.com>
2022-05-05 02:52:00 +0100

    Docker Compose app with containers based on SQL Server 2019, aspnetcore and nginx images

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using ExampleApp.Models;
+using Microsoft.Extensions.Configuration;
+namespace ExampleApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+        private string message;
+        public HomeController(IRepository repo, IConfiguration config)
+        {
+            repository = repo;
+            message = config["HOSTNAME"] ?? "Essential Docker";
+        }
+        public IActionResult Index()
+        {
+            ViewBag.Message = message;
+            return View(repository.Products);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExampleApp.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ExampleApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        private IConfiguration Configuration
+        {
+            get;
+            set;
+        }
+
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContext<ProductDbContext>(optionsBuilder =>
+            {
+
+                optionsBuilder.UseSqlServer(Configuration.GetConnectionString("ProductDB"))
+                    .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+            services.AddTransient<IRepository, ProductRepository>();
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ProductDbContext context, ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStatusCodePages();
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseStaticFiles();
+
+            logger.LogInformation(Configuration.GetConnectionString("ProductDB"));
+            SeedData.EnsurePopulated(context);
+
+        }
+    }
+}
